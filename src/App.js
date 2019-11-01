9aa90e067d9688115ffe0e5326162484b3566c87
George Webster <george.webster@nationwide.co.uk>
2019-11-01 15:27:49 +0000

    fixed return problems

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>David's a stunner</div>;
-  return <div>goose</div>;
-  return <div>ducky</div>;
+  return (
+    <>
+      <div>David's a stunner</div>;
+      <div>goose</div>;
+      <div>ducky</div>
+    </>
+  );
 }
 
 export default App;

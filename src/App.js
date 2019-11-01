1add5be535e5da4e5d6252ab53d196a617109633
DavidSharp <david.sharp@nationwide.co.uk>
2019-11-01 16:11:53 +0000

    added in pages and block components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./block/Button";
+import Welcome from "./pages/Welcome";
 
 function App() {
+  // Change pageNumber to a number for you to work on and ONLY see your page.
+  // D 1, A 2, O 3, G 4
+  var pageNumber = 0;
   return (
     <>
-      <div>David's a stunner</div>;
-      <div>goose</div>;
-      <div>ducky</div>
+      <Button name="David" />
+      <Button name="Arbind" />
+      <Button name="Omar" />
+      <Button name="George" />
+      {/*Ifstatement below to only show welcome page if pageNumber is 0*/}
+      {pageNumber === 0 && <Welcome />}
     </>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../block/Button";
+
+function Welcome() {
+  return (
+    <>
+      <h1>Welcome page</h1>
+      <p>Welcome to our amazing TDP calendar page</p>
+      <h2>Please enter your name</h2>
+      <input></input>
+      <Button name="submit" />
+    </>
+  );
+}
+
+export default Welcome;

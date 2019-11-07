458688460dc86292b89767eb719e381796f4cd0c
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2019-11-07 17:09:45 +0000

    get rid of test data

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,7 +6,6 @@ function Welcome() {
   return (
     <>
       <Header header="TDP Events Page"/>
-      <Header header="Other test"/>
       <p>Welcome to our amazing TDP calendar page</p>
       <h2>Please enter your name</h2>
       <input></input>

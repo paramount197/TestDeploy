c91f178388a8221b319d6546a7b961de362e4d02
Arabind Dhakal <arabind_97@hotmail.com>
2019-11-08 13:07:31 +0000

    FIXED Complete before 1st meting with mark

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Button from "./block/Button";
 import Welcome from "./pages/Welcome";
 import Header from "./block/Header";
+import { green } from "ansi-colors";
 
 function App() {
   // Change pageNumber to a number for you to work on and ONLY see your page.
@@ -11,11 +12,28 @@ function App() {
   var pageNumber = 0;
   var date = new Date();
   var getdate = date.toLocaleString();
+  const divStyle = {
+    color: 'RED',
+
+    // backgroundImage: 'url(' + imgUrl + ')',
+  };
+
   
   return (
     <>
       {/*Ifstatement below to only show welcome page if pageNumber is 0*/}
+      <Button name={getdate}/>
+      {/* <div style={divStyle}>SIGN IN</div>; */}
+      <div className="buttonDiv">
+          <Button name="SIGN IN" />
+          <Button name="REGISTER" />
+          <Button name="USER-GUIDE" />
+      </div>
       {pageNumber === 0 && <Welcome />}
+      
+    
+
+
 
     
 

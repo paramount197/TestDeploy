1248a17eaf1a7e06dc6572e79f21a969712726cf
DavidSharp <david.sharp@nationwide.co.uk>
2019-11-12 22:11:45 +0000

    Notes added

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Header from "../block/Header";
 import Button from "../block/Button";
+import Intro from "../block/Intro";
 
 // Questions
 // Would you pass header name hear or from app.js as props
+
+//Actions
+// pass intro text as props
+// Review input fields in React
+// Create the input fields
+// create Button
+// if time try and store the data in local storage and show it om new page
 
 const Registration = props => {
   return (
     <>
       <Header header="Registration page" />;
+      <Intro />
       <Button />
     </>
   );

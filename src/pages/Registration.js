6aaa94f7e3222640df28ff99573b7dad165b6f87
DavidSharp <david.sharp@nationwide.co.uk>
2019-11-12 22:07:26 +0000

    page created

---
+++ src/pages/Registration.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../block/Header";
+import Button from "../block/Button";
+
+// Questions
+// Would you pass header name hear or from app.js as props
+
+const Registration = props => {
+  return (
+    <>
+      <Header header="Registration page" />;
+      <Button />
+    </>
+  );
+};
+
+export default Registration;

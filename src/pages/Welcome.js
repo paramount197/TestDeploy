5e2ec23ccbe9a32bb39201fe5e027bd7259d821b
DavidSharp <david.sharp@nationwide.co.uk>
2019-11-20 19:17:01 +0000

    routing added, need to review if Navlink was right

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,6 +3,7 @@ import Header from "../block/Header";
 import Intro from "../block/Intro";
 import Events from "../block/Events";
 import Button from "../block/Button";
+import { NavLink } from "react-router-dom";
 
 // This is an array of objects that currently holds the event details.
 // This will eventually be provided via the eventBrite API
@@ -40,6 +41,7 @@ function Welcome() {
       <Intro intro="Welcome to our TDP Event booking" />
       <div className="buttonDiv">
         <Button name="SIGN IN" />
+        <NavLink to="/registration">Sign In</NavLink>
         <Button name="REGISTER" />
         <Button name="USER-GUIDE" />
       </div>

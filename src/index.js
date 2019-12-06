73906b6769ffd515f40c5f54093da1c1e5757b65
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2019-12-06 10:12:43 +0000

    removed ls

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const routing = (
        <Route exact path="/" component={App} />
        <Route path="/welcome" component={Welcome} />
        <Route path="/registration" component={Registration} />
-       <Route path="/usersguide" component={UsersGuide} />ls
+       <Route path="/usersguide" component={UsersGuide} />
        <Route component={NotFound} />
      </Switch>
    </Router>

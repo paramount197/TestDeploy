757c7cecbf6d7e6cf45ddc5b17a6746c12fe3ded
DavidSharp <david.sharp@nationwide.co.uk>
2019-12-10 22:58:34 +0000

    tidy css

--- src/block/ListItem.js
+++ src/block/ListItem.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/listitem.css";
 
 function ListItem(props) {
   return (
-    <li>
+    <li className={props.className}>
       {props.eventName} on the {props.eventDate} at {props.eventLocation}
     </li>
   );

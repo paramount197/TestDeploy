2a1ac5b3b6624ea017579ccfa2fced7dd32cabf3
DavidSharp <david.sharp@nationwide.co.uk>
2020-01-14 17:18:25 +0000

    broken post

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Intro from "./block/Intro";
 import Events from "./block/Events";
 import Button from "./block/Button";
 import { NavLink } from "react-router-dom";
+import Axios from "./Axios/Axios";
 
 const events = require("./data/events.json");
 //imports all the data from a json file accessible everywhere
@@ -16,20 +17,21 @@ function App() {
     <>
       <Header header="TDP Events" />
       <Intro intro="Welcome to our TDP Event booking" />
-      
-        <div className="buttonDivCont">
-          <div className="buttonDiv">
-            <NavLink to="/signin">
-              <Button name="SIGN IN" className="homeButton" />
-            </NavLink>
-            <NavLink to="/registration">
-              <Button name="REGISTER" className="homeButton" />
-            </NavLink>
-            <NavLink to="/usersguide">
-              <Button name="USER GUIDE" className="homeButton userGuide" />
-            </NavLink>
-          </div>
-        
+      <Axios />
+
+      <div className="buttonDivCont">
+        <div className="buttonDiv">
+          <NavLink to="/signin">
+            <Button name="SIGN IN" className="homeButton" />
+          </NavLink>
+          <NavLink to="/registration">
+            <Button name="REGISTER" className="homeButton" />
+          </NavLink>
+          <NavLink to="/usersguide">
+            <Button name="USER GUIDE" className="homeButton userGuide" />
+          </NavLink>
+        </div>
+
         <div className="eventList">
           <p className="eventTitle">Upcoming Events</p>
           <Events

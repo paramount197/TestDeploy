cc707847c4fea649c894508f3c9841a0b5bc7cc0
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2020-01-15 15:29:51 +0000

    Created a Blurb for the Home Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./block/Header";
 import Intro from "./block/Intro";
+import Blurb from "./block/Blurb";
 import Events from "./block/Events";
 import Button from "./block/Button";
 import { NavLink } from "react-router-dom";
@@ -16,20 +17,21 @@ function App() {
     <>
       <Header header="TDP Events" />
       <Intro intro="Welcome to our TDP Event booking" />
-      
+      <Blurb Blurb="Hello and welcome to the TDP Events Webpage. Here you'll find all current and future TDP events.
+                    You'll be able to confirm your attendence to the events via this webpage. Are you new user? Click 'Register' to sign up. 
+                    If you are a returning user, click 'Sign In' to continue. For futher guidence, see the 'Users-Guide'." />
         <div className="buttonDivCont">
-          <div className="buttonDiv">
-            <NavLink to="/signin">
-              <Button name="SIGN IN" className="homeButton" />
-            </NavLink>
-            <NavLink to="/registration">
-              <Button name="REGISTER" className="homeButton" />
-            </NavLink>
-            <NavLink to="/usersguide">
-              <Button name="USER GUIDE" className="homeButton userGuide" />
-            </NavLink>
-          </div>
-        
+        <div className="buttonDiv">
+          <NavLink to="/signin">
+            <Button name="SIGN IN" className="homeButton" />
+          </NavLink>
+          <NavLink to="/registration">
+            <Button name="REGISTER" className="homeButton" />
+          </NavLink>
+          <NavLink to="/usersguide">
+            <Button name="USER GUIDE" className="homeButton userGuide" />
+          </NavLink>
+        </div>
         <div className="eventList">
           <p className="eventTitle">Upcoming Events</p>
           <Events
@@ -38,6 +40,7 @@ function App() {
           />
         </div>
       </div>
+
     </>
   );
 }
---
+++ src/block/Blurb.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Blurb(props) {
+    return <p>{props.Blurb}</p>;
+}
+
+export default Blurb;

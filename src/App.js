9b2de467a212065f8ad07ca79a36ad593dfae14a
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2020-01-16 16:22:49 +0000

    Line spacing sorted

--- src/App.js
+++ src/App.js
@@ -17,10 +17,12 @@ function App() {
     <>
       <Header header="TDP Events" />
       <Intro intro="Welcome to our TDP Event booking" />
-      <Blurb Blurb="Hello and welcome to the TDP Events Webpage. Here you'll find all current and future TDP events.
-                    You'll be able to confirm your attendance to the events via this webpage. Are you new user? Click 'Register' to sign up. 
-                    If you are a returning user, click 'Sign In' to continue. For further guidance, see the 'Users-Guide'." />
-        <div className="buttonDivCont">
+      <Blurb
+        line1="Hello and welcome to the TDP Events Webpage. Here you'll find all current and future TDP events."
+        line2="You'll be able to confirm your attendance to the events via this webpage. Are you new user? Click 'Register' to sign up."
+        line3="If you are a returning user, click 'Sign In' to continue. For further guidance, see the 'Users-Guide'."
+      />
+      <div className="buttonDivCont">
         <div className="buttonDiv">
           <NavLink to="/signin">
             <Button name="SIGN IN" className="homeButton" />
--- src/block/Blurb.js
+++ src/block/Blurb.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Blurb(props) {
-    return <p>{props.Blurb}</p>;
+    return <>
+        <p>{props.line1}</p>
+        <p>{props.line2}</p>
+        <p>{props.line3}</p>
+    </>
 }
 
 export default Blurb;

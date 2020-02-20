2ffc23c46d248d2db4e01e90c9cef1d09cf37bc4
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2020-02-20 18:27:51 +0000

    Have admeded the Users Guide Page

--- src/pages/UsersGuide.js
+++ src/pages/UsersGuide.js
@@ -2,12 +2,11 @@ import React from "react";
 import Header from "../block/Header";
 import Intro from "../block/Intro";
 import Blurb from "../block/Blurb";
-import "../styles/usersguide.css";
-import Image from "../block/Image";
 import Image1 from "../images/placeholder.png";
 import Image2 from "../images/placeholder2.png";
 import Image3 from "../images/placeholder3.png";
 import Image4 from "../images/placeholder4.png";
+import UserGuideTextBox from "../block/UserGuideTextBox";
 
 
 function UsersGuide() {
@@ -18,65 +17,54 @@ function UsersGuide() {
             <Intro intro="Welcome to the UserGuide Page." />
             <Blurb blurb="Here you'll be able to learn how to navigate the TDP Events Webpage" />
          </div>
-         <body>
-            <div styleName="display" />
-            <Image
+         <UserGuideTextBox
+               Image
                src={Image1}
-               styleName="display"
-               alt="Placeholder"
-            />
-            <Image
+               alt="Test Image 1"
+               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     used do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                     deserunt mollit anim id est laborum."
+         />
+         <UserGuideTextBox
+               Image
                src={Image2}
-               styleName="display"
-               alt="Placeholder2"
-            />
-            <Image
+               alt="Test Image 2"
+               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     used do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                     deserunt mollit anim id est laborum."
+         />
+         <UserGuideTextBox
+               Image
                src={Image3}
-               styleName="display"
-               alt="Placeholder3"
-            />
-            <Image
+               alt="Test Image 3"
+               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     used do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                     deserunt mollit anim id est laborum."
+         />
+         <UserGuideTextBox
+               Image
                src={Image4}
-               styleName="display"
-               alt="Placeholder4"
-            />
-            <div className="box1">
-               <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
-                                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
-            </div>
-            <div className="box2">
-               <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
-            </div>
-            <div className="box3">
-               <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
-            </div>
-            <div className="box4">
-               <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
-            </div>
-         </body>
+               alt="Test Image 4"
+               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     used do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laborin
+                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                     deserunt mollit anim id est laborum."
+         />
       </>
    );
 }

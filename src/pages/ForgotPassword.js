5f7f52d69fc1529622241489fdcb7b96a5af731b
Omar Suleman <omarsuleman@Omars-MacBook-Pro.local>
2020-02-28 17:58:20 +0000

    pre commit prior to additional work form Omar

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,8 +1,8 @@
 import React from "react";
+import axios from "axios";
 import Header from "../block/Header";
 import Intro from "../block/Intro";
 import Input from "../block/Input";
-import Dropdown from "../block/Dropdown";
 import Submit from "../block/Submit";
 import { NavLink } from "react-router-dom";
 import "../styles/forgotpassword.css";
@@ -10,48 +10,112 @@ import "../styles/input.css";
 
 const securityQuestions = require("../data/securityQuestions.json");
 
-function ForgotPassword(props) {
-  return (
-    <>
-      <Header header="Forgot Password?" />
-      <div class="main">
-        <div class="row">
-          <div class="form-p">
-            <Intro intro="Please enter your email address and answer your chosen security question below" />
-            <form>
-              <Input
-                type="email"
-                placeholder="Enter Email Address"
-                name="Email Address"
-                required
-              />
-            </form>
-            <div className="securityQuestions">
-              <label>Security Question</label>
-              <select>
-                <Dropdown
-                  intakeProgrammeDetails={
-                    securityQuestions.securityQuestion.Questions
-                  }
-                />
-              </select>
-              <form>
-                <Input
-                  type="text"
-                  placeholder="Write Answer Here"
-                  name="Answer"
-                  required
-                />
-                <NavLink to="/signin">
+class ForgotPassword extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailHasBeenInput: false,
+      email: "",
+      securityQuestion: "",
+      securityAnswer: "",
+      userAnswer: "",
+      response: ""
+    };
+  }
+
+  onChange = e => {
+    console.log('onchange')
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  getUserData() {
+    axios
+      .get(`http://localhost:4000/users?id=${this.state.email}`)
+      .then(result => {
+        if (result.data.length !== 0) {
+          //if user does exist (result.data is NOT empty)
+          console.log(result.data)
+          this.setState({
+            emailHasBeenInput: true,
+            securityQuestion: result.data[0].securityQuestion,
+            securityAnswer: result.data[0].securityAnswer
+          });
+        }
+        else {
+          //if user doesn't exist 
+          this.handleIncorrectDetails();
+        }
+      });
+  }
+
+  handleIncorrectDetails = () => {
+    this.setState({
+      response: "Please check you have entered your details correctly"
+    });
+  };
+
+  handleSecurityQuestion() {
+    console.log(this.state)
+    if (this.state.userAnswer === this.state.securityAnswer) {
+      console.log('success')
+    }
+    else {
+      console.log('fail')
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+        <Header header="Forgot Password?" />
+        <div class="main">
+          <div class="row">
+            <div class="form-p">
+              <Intro intro="Please enter your email address and answer your chosen security question below" />
+              {!this.state.emailHasBeenInput && (
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    this.getUserData();
+                  }}>
+                  <Input
+                    type="email"
+                    placeholder="Enter Email Address"
+                    name="email"
+                    required
+                    onChange={this.onChange}
+                  />
                   <Submit />
-                </NavLink>
-              </form>
+                  <p className="response">{this.state.response}</p>
+                </form>
+              )}
+              {this.state.emailHasBeenInput && (
+                <div className="securityQuestions">
+                  <label>Security Question</label>
+                  <p>{this.state.securityQuestion}</p>
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      this.handleSecurityQuestion();
+                    }}>
+                    <Input
+                      type="text"
+                      placeholder="Write Answer Here"
+                      name="userAnswer"
+                      required
+                      onChange={this.onChange}
+                    />
+                    <Submit />
+                  </form>
+                </div>
+              )}
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default ForgotPassword;

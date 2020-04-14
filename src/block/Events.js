02ddc52474b24363881ff1f09394302954e06a9b
DavidSharp <david.sharp@nationwide.co.uk>
2020-04-14 21:55:46 +0100

    working, needs tidy up, actions at top

--- src/block/Events.js
+++ src/block/Events.js
@@ -3,14 +3,14 @@ import ListItem from "./ListItem";
 import axios from "axios";
 
 // will need to change no. of attendees to an integer!!
+// Do something so they can't book when full!
+// remove console log
 
 const Events = (props) => {
   const fullEventCheck = (event) => {
     if (event.booked.length >= event.attendees) {
-      return true; //+ console.log("this is full", event.booked.length, event.attendees)
+      return true;
     } else return false;
-    // false +
-    // console.log("there is space", event.booked.length, event.attendees)
   };
 
   const buttonClicked = (event) => {
@@ -35,19 +35,6 @@ const Events = (props) => {
       attendees={event.attendees}
       showButton={props.showButton}
       buttonClick={buttonClicked.bind(null, event)}
-      //{fullEventCheck.bind(null, event)}
-      //{buttonClicked.bind(null, event)}
-      // fullEventCheck();
-      //   if (fullEventCheck === true) {
-      //     event.booked.push(props.currentUserEmail);
-      //     axios.patch(`http://localhost:4000/events/${event.id}`, {
-      //       booked: event.booked,
-      //     });
-      //     window.location.reload();
-      //   } else {
-      //     console.log("This event is full");
-      //   }
-      // }}
       buttonText={fullEventCheck(event) ? "Full" : "Book"}
     />
   ));

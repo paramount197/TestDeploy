385d7b470120f07127305d6ab534fc3e7794cb8d
George Webster <george.webster@nationwide.co.uk>
2020-04-21 22:04:14 +0100

    fixed footer

--- src/block/Footer.js
+++ src/block/Footer.js
@@ -4,10 +4,9 @@ import "../styles/footer.css";
 const Footer = () => (
   <footer className="footer">
     <p>
-      Any issues please contact:{" "}
+      Any issues please contact:
       <a href="technologydevelopmentprogramme@nationwide.co.uk">TDP Mailbox</a>
     </p>
-    <p>Any questions, any time please contact Mark Bates on Teams</p>
   </footer>
 );
 

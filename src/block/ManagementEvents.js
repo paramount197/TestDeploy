4429abd74917e64bd3cbf52dd5f7bfea046dd795
Omar Suleman <omarsuleman@omars-mbp.home>
2020-04-28 16:39:10 +0100

    table

--- src/block/ManagementEvents.js
+++ src/block/ManagementEvents.js
@@ -5,13 +5,6 @@ const ManagementEvents = (props) => {
     return props.eventsDetail.map((event) => (
         <ListItem
             className="event"
-            //eventName={event.name}
-            //text="on the"
-            //eventDate={event.date}
-            //text2="at "
-            //eventLocation={event.location}
-            //text3="Currently attending "
-            //booked={event.booked}
             text={`${event.name} on the ${event.date} at ${event.location} Currently Attending - ${event.booked}`} />
     ));
 };
---
+++ src/block/managementTable.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+
+
+<Table striped bordered hover variant="dark">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Event</th>
+            <th>Date</th>
+            <th>Location</th>
+            <th>Currently attending</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>{event.name}</td>
+            <td>{event.date}</td>
+            <td>{event.location}</td>
+            <td>{event.booked}}</td>
+        </tr>
+    </tbody>
+</Table >
+
+export defult managementTable;

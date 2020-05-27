a66b9967d6f70ca56f5c054d804821375e1bf68c
Arabind <arabind_97@hotmail.com>
2020-05-27 18:07:07 +0100

    Updates

---
+++ precache-manifest.919d629586c121bbe11bef3cb53d28f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2677707a436800c3376f569e41d0200a",
+    "url": "/TestDeploy/index.html"
+  },
+  {
+    "revision": "84fa2c4d9282d84d64ce",
+    "url": "/TestDeploy/static/css/main.550874da.chunk.css"
+  },
+  {
+    "revision": "e0b86d572558c63019c9",
+    "url": "/TestDeploy/static/js/2.f0ae9ff9.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/TestDeploy/static/js/2.f0ae9ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fa2c4d9282d84d64ce",
+    "url": "/TestDeploy/static/js/main.fb0eabc4.chunk.js"
+  },
+  {
+    "revision": "644c1dfeb591ed431c98",
+    "url": "/TestDeploy/static/js/runtime-main.7bd43450.js"
+  },
+  {
+    "revision": "4d5de05bef49077a051684c5cee38722",
+    "url": "/TestDeploy/static/media/Img1.4d5de05b.jpeg"
+  },
+  {
+    "revision": "1ca0f1070578b3bd9eae7fecff7543d6",
+    "url": "/TestDeploy/static/media/Img2.1ca0f107.jpg"
+  },
+  {
+    "revision": "14d042cd8fe218e922687ee8d2f1517b",
+    "url": "/TestDeploy/static/media/Img3.14d042cd.jpg"
+  },
+  {
+    "revision": "5d8906c48188c2eb52151b8d9337a795",
+    "url": "/TestDeploy/static/media/Img4.5d8906c4.jpg"
+  },
+  {
+    "revision": "8668efdf513b1b85b8480d786afc659b",
+    "url": "/TestDeploy/static/media/NewNationwideLogo.8668efdf.jpeg"
+  },
+  {
+    "revision": "75fd4d9a2042039e8998b76ff14eae86",
+    "url": "/TestDeploy/static/media/placeholder.75fd4d9a.png"
+  },
+  {
+    "revision": "302659e37a280404236922243e3f6616",
+    "url": "/TestDeploy/static/media/placeholder2.302659e3.png"
+  },
+  {
+    "revision": "f0cb45feb6a824c22e38c535ac1e58ce",
+    "url": "/TestDeploy/static/media/placeholder3.f0cb45fe.png"
+  },
+  {
+    "revision": "6cf6664840a7b119a2c8a3f2a499d240",
+    "url": "/TestDeploy/static/media/placeholder4.6cf66648.png"
+  }
+]);
